cea9cdaaf16798c9146eb5e588070f7e19253731
Okan Acıoğlu <okanacioglu@gmail.com>
2022-12-02 14:14:17 +0300

    router-link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
-
+import Navigation from './routes/navigation/navigation.component'
 import Home from './routes/home/home.component'
 const App = () => {
+  const Shop = () => {
+    return <h1>Shop Component</h1>
+  }
+
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
   )
   // const categories = [
