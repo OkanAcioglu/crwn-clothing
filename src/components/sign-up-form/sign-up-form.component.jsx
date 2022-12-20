a8c11d72179941ec8dc0242a605d520aa4dbd1b7
Okan Acıoğlu <okanacioglu@gmail.com>
2022-12-20 15:46:45 +0300

    sign-up-form-setup

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,17 @@
+const SignUpForm = () => {
+  return (
+    <div>
+      <h1>Sign up with your email and password</h1>
+      <form>
+        <label htmlFor=''></label>
+        <input required />
+        <label htmlFor=''></label>
+        <input required />
+        <label htmlFor=''></label>
+        <input required />
+        <label htmlFor=''></label>
+        <input required />
+      </form>
+    </div>
+  )
+}
